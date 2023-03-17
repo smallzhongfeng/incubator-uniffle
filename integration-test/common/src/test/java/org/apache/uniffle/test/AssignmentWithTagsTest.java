87c0cd22d847a0e8ce2b5eb2e0a52fac7d58837f
smallzhongfeng <zhongjingxiong@didiglobal.com>
2023-03-17 14:04:05 +0800

    fix

--- integration-test/common/src/test/java/org/apache/uniffle/test/AssignmentWithTagsTest.java
+++ integration-test/common/src/test/java/org/apache/uniffle/test/AssignmentWithTagsTest.java
@@ -21,6 +21,7 @@ import java.io.File;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
